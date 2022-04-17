53b1747b7520e506f640b54d3f51b4d7eecd7c2e
MuhammadHarisSikander <harissikander786@gmail.com>
2022-04-17 06:26:20 +0500

    first commit

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,266 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionSplitProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import {CircularProgress} from '@mui/material'
+
+
+
+const propTypes = {
+  ...SectionSplitProps.types
+}
+
+const defaultProps = {
+  ...SectionSplitProps.defaults
+}
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-split section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-split-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const splitClasses = classNames(
+    'split-wrap',
+    invertMobile && 'invert-mobile',
+    invertDesktop && 'invert-desktop',
+    alignTop && 'align-top'
+  );
+
+  const sectionHeader = {
+    title: 'Workflow that just works',
+    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
+  };
+  const percentage = 50;
+
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+
+
+      <div className="container">
+       <CircularProgress variant="determinate" value={60} />
+        {/* <div style={{ width: 150, }}>
+
+          <CircularProgressbar
+            value={percentage}
+            text={`${percentage}%`}
+            styles={{
+              // Customize the root svg element
+              root: {},
+              // Customize the path, i.e. the "completed progress"
+              path: {
+                // Path color
+                stroke: `rgba(62, 152, 199, ${percentage / 100})`,
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: 'butt',
+                // Customize transition animation
+                transition: 'stroke-dashoffset 0.9s ease 0s',
+                // Rotate the path
+                // transform: 'rotate(0.25turn)',
+                transformOrigin: 'center center',
+              },
+              // Customize the circle behind the path, i.e. the "total progress"
+              trail: {
+                // Trail color
+                stroke: '#d6d6d6',
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: 'butt',
+                // Rotate the trail
+                transform: 'rotate(0.25turn)',
+                transformOrigin: 'center center',
+              },
+              // Customize the text
+              text: {
+                // Text color
+                fill: '#f88',
+                // Text size
+                fontSize: '10px',
+                textAlign: 'center',
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'center',
+                padding:1000,
+                margin:50,
+                border:'2px solid red'
+              },
+              // Customize background - only used when the `background` prop is true
+              background: {
+                // fill: '#3e98c7',
+              },
+            }}
+          />
+        </div>
+
+         <div style={{ width: 100 }}>
+
+          <CircularProgressbar
+            value={percentage}
+            text={`${percentage}%`}
+            styles={{
+              // Customize the root svg element
+              root: {},
+              // Customize the path, i.e. the "completed progress"
+              path: {
+                // Path color
+                stroke: `rgba(62, 152, 199, ${percentage / 100})`,
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: 'butt',
+                // Customize transition animation
+                transition: 'stroke-dashoffset 0.5s ease 0s',
+                // Rotate the path
+                // transform: 'rotate(0.25turn)',
+                transformOrigin: 'center center',
+              },
+              // Customize the circle behind the path, i.e. the "total progress"
+              trail: {
+                // Trail color
+                stroke: '#d6d6d6',
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: 'butt',
+                // Rotate the trail
+                transform: 'rotate(0.25turn)',
+                transformOrigin: 'center center',
+              },
+              // Customize the text
+              text: {
+                // Text color
+                fill: '#f88',
+                // Text size
+                fontSize: '16px',
+                textAlign: 'center',
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'center'
+              },
+              // Customize background - only used when the `background` prop is true
+              background: {
+                fill: '#3e98c7',
+              },
+            }}
+          />
+        </div> */}
+        {/* <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/features-split-image-01.png')}
+                  alt="Features split 01"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/features-split-image-02.png')}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Lightning fast workflow
+                  </div>
+                <h3 className="mt-0 mb-12">
+                  Data-driven insights
+                  </h3>
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+              </div>
+              <div className={
+                classNames(
+                  'split-item-image center-content-mobile reveal-from-bottom',
+                  imageFill && 'split-item-image-fill'
+                )}
+                data-reveal-container=".split-item">
+                <Image
+                  src={require('./../../assets/images/features-split-image-03.png')}
+                  alt="Features split 03"
+                  width={528}
+                  height={396} />
+              </div>
+            </div>
+
+          </div>
+        </div> */}
+      </div>
+    </section>
+  );
+}
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
